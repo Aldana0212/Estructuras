0d359c3c803447fa33da2fc198a109047e9fd8f5
Aldana0212 <37188742+Aldana0212@users.noreply.github.com>
2018-09-04 16:46:54 -0600

    Update Suma.c

--- Suma.c
+++ Suma.c
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* 
- * File:   main.c
- * Author: cristian
- *
- * Created on 31 de agosto de 2018, 06:30 AM
- */
 
 #include <stdio.h>
 #include <stdlib.h>
